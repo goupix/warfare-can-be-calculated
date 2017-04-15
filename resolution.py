e187bd645dc3fe480df440c7aeb7188ff6e22324
goupix <meghdecroocq@gmail.com>
2017-04-15 17:52:31 +0200

    ajusting parameters

--- resolution.py
+++ resolution.py
@@ -5,10 +5,8 @@ def F(r,S,ku,kv,dx,Du,Dv, u, v, i):
 
 	newu=r*u[i]*(1-u[i]/S)-kv*v[i]+Du*((u[i+1]-2*u[i]+u[i-1])/(dx**2))
 	newv=r*v[i]*(1-v[i]/S)-ku*u[i]+Dv*((v[i+1]-2*v[i]+v[i-1])/(dx**2))
-	if(newu<0):
-		newu=0
-	if(newv<0):
-		newv=0
+	print(Du*((u[i+1]-2*u[i]+u[i-1])/(dx**2)),Dv*((v[i+1]-2*v[i]+v[i-1])/(dx**2)))
+
 
 	return(newu, newv)
 
@@ -31,9 +29,16 @@ def euler(r,S,ku,kv,dx,Du,Dv,ti,tf, L, h, F, u_init, v_init):
 		for j in range(1,D-1):
 
 			fu,fv=F(r,S,ku,kv,dx,Du,Dv, u[i], v[i], j)
-			
-			u[i+1][j]=round(h*fu+u[i][j],2)
-			v[i+1][j]=round(h*fv+v[i][j],2)
+
+			if((h*fu+u[i][j])<0):
+				u[i+1][j]=0
+			else:
+				u[i+1][j]=h*fu+u[i][j]
+
+			if((h*fv+v[i][j])<0):
+				v[i+1][j]=0
+			else:
+				v[i+1][j]=h*fv+v[i][j]
 
 	return(temps, u,v)
 
@@ -41,10 +46,10 @@ u_init=[0]*50+[200]*50
 v_init=[200]*50+[0]*50
 
 
-temps,u,v=euler(0.01,1000,0.1,0.2,0.1,0.1,0.1,0,100,10,1, F, u_init, v_init)
+
+temps,u,v=euler(0.01,1000,0,10,1,0.1,0.1,0,100,100,1, F, u_init, v_init)
 
 
-print(temps,u[0])
-plt.plot(temps,u[6])
-plt.plot(temps,v[6])
+plt.plot(temps,u[90])
+plt.plot(temps,v[90])
 plt.show()
